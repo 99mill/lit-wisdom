2706d40c6f704b98f29616cf3a005b4bb40d5ea3
llorenmillerr <llorenmillerr@gmail.com>
2018-06-22 18:19:51 -0700

    refactored, added to head (meta, favicon), logo img, stripe gradient

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
+// constant variables
 const newQuote = document.querySelector('.generate');
 const quote = document.querySelector('.quote');
 const author = document.querySelector('.author');
 const url = 'https://random-quote-generator.herokuapp.com/api/quotes/random';
 
+// load quote on load or fetch new quote when prompted
 window.onload = generateQuote;
 newQuote.addEventListener('click', generateQuote);
 
+// fetch quote from api and display new text
 function generateQuote() {
     fetch(url)
     .then(r => r.json())
-    .then(function(data) {
-        var text = data.quote;
+    .then(data => {
         quote.textContent = data.quote;
         author.textContent = data.author;  
     })
-    .catch(function(error) {
-      console.log(error);
-    });
-    console.log('works');
-    
+    .catch(e => console.log(e));
 }
-
-
-
-
-
-
-
-//
